c0c75c17ee36624a4d5374876a13b432600780f4
roman.atachiants@careem.com <roman.atachiants@careem.com>
2021-03-31 11:31:55 +0400

    exposed duration

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
-module github.com/grab/async
+module github.com/kelindar/async
 
-go 1.12
+go 1.16
 
 require (
-	github.com/stretchr/testify v1.3.0
-	golang.org/x/time v0.0.0-20191024005414-555d28b269f0
+	github.com/stretchr/testify v1.7.0
+	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba
 )
--- task.go
+++ task.go
@@ -1,4 +1,5 @@
 // Copyright 2019 Grabtaxi Holdings PTE LTE (GRAB), All rights reserved.
+// Copyright 2021 Roman Atachiants
 // Use of this source code is governed by an MIT-style license that can be found in the LICENSE file
 
 package async
@@ -53,6 +54,7 @@ type Task interface {
 	State() State
 	Outcome() (interface{}, error)
 	ContinueWith(ctx context.Context, nextAction func(interface{}, error) (interface{}, error)) Task
+	Duration() time.Duration
 }
 
 // NewTask creates a new task.
