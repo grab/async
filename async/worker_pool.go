0c5ecb53c15c51d7d92d2e80deb19a4bc9c90ee2
James Tran <4ccount.manager@gmail.com>
2022-05-31 21:41:26 +0800

    initial implementation for Worker Pool

---
+++ async/worker_pool.go
@@ -0,0 +1,360 @@
+package async
+
+import (
+	"context"
+	"runtime"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/gammazero/deque"
+)
+
+const (
+	// If all workers stay idle for at least this duration, then kill one.
+	idleTimeout = 2 * time.Second
+)
+
+// queuedTask represents a task to be executed using the provided context
+type queuedTask struct {
+	task SilentTask
+	ctx  context.Context
+}
+
+// run executes the queued task using the provided context
+func (qt *queuedTask) run() {
+	qt.task.Execute(qt.ctx)
+	// Worker is already running on an independent goroutine. We must wait for
+	// the task to complete before releasing the worker for the next task.
+	qt.task.Wait()
+}
+
+// cancel cancels the queued task
+func (qt *queuedTask) cancel() {
+	qt.task.Cancel()
+}
+
+// WorkerPool is similar to a thread pool in Java, where the number of concurrent
+// goroutines processing requests does not exceed the configured maximum.
+type WorkerPool struct {
+	maxSize int
+
+	taskQueue          chan *queuedTask
+	workerQueue        chan *queuedTask
+	stoppedChan        chan struct{}
+	stopSignal         chan struct{}
+	waitingQueue       deque.Deque
+	stopLock           sync.Mutex
+	stopOnce           sync.Once
+	stopped            bool
+	pendingSize        int32
+	waitBeforeShutdown bool
+}
+
+// New creates and starts a pool of worker goroutines.
+//
+// `maxSize` specifies the maximum number of workers that can execute tasks
+// concurrently. When there's no incoming tasks, workers get killed 1-by-1
+// until there's no remaining workers.
+func New(maxSize int) *WorkerPool {
+	// Default is number of CPU cores.
+	if maxSize < 1 {
+		maxSize = runtime.NumCPU()
+	}
+
+	pool := &WorkerPool{
+		maxSize:     maxSize,
+		taskQueue:   make(chan *queuedTask, 1),
+		workerQueue: make(chan *queuedTask),
+		stopSignal:  make(chan struct{}),
+		stoppedChan: make(chan struct{}),
+	}
+
+	// Start the task dispatcher.
+	go pool.dispatch()
+
+	return pool
+}
+
+// Size returns the maximum number of concurrent workers.
+func (p *WorkerPool) Size() int {
+	return p.maxSize
+}
+
+// Stop stops this worker pool and waits for the running tasks to complete. Pending tasks
+// that are still in the queue will get cancelled. New tasks must not be submitted to the
+// worker pool after calling Stop().
+//
+// Note: to avoid memory leak, clients MUST always call Stop() or StopWait() when this
+// worker pool is no longer needed.
+func (p *WorkerPool) Stop() {
+	p.stop(false)
+}
+
+// StopWait stops this worker pool and waits for the running tasks + all queued tasks to
+// complete. New tasks must not be submitted to the worker pool after calling Stop().
+//
+// Note: to avoid memory leak, clients MUST always call Stop() or StopWait() when this
+// worker pool is no longer needed.
+func (p *WorkerPool) StopWait() {
+	p.stop(true)
+}
+
+// Stopped returns true if this worker pool has been stopped.
+func (p *WorkerPool) Stopped() bool {
+	p.stopLock.Lock()
+	defer p.stopLock.Unlock()
+
+	return p.stopped
+}
+
+// Submit enqueues a function for a worker to execute.
+//
+// Submit will not block regardless of the number of tasks submitted. Each task is given
+// to an available worker or to a newly started worker. If there's no available workers,
+// and no new workers can be created due to the configured maximum, then the task is put
+// onto a waiting queue.
+//
+// When the waiting queue is not empty, incoming tasks will go into the queue immediately.
+// Tasks are removed from the waiting queue as workers become available.
+//
+// As long as no new tasks arrive, one idle worker will get killed periodically until no
+// more workers are left. Since starting new goroutines is cheap & quick, there's no need
+// to retain idle workers indefinitely.
+func (p *WorkerPool) Submit(ctx context.Context, task SilentTask) {
+	if task != nil {
+		p.taskQueue <- &queuedTask{
+			task: task,
+			ctx:  ctx,
+		}
+	}
+}
+
+// WaitingQueueSize returns the count of tasks in the waiting queue.
+func (p *WorkerPool) WaitingQueueSize() int {
+	return int(atomic.LoadInt32(&p.pendingSize))
+}
+
+// Pause causes all workers to wait on the given context, thereby making them unavailable
+// to run tasks. Pause returns when all workers are waiting. Tasks can still be queued but
+// won't get executed until `ctx` is cancelled or times out.
+//
+// Calling Pause when the worker pool is already paused causes Pause to wait until previous
+// pauses are cancelled. This allows a goroutine to take control of pausing and un-pausing
+// the pool as soon as other goroutines have un-paused it.
+//
+// When this worker pool is stopped, workers are un-paused and queued tasks may be executed
+// during StopWait.
+func (p *WorkerPool) Pause(ctx context.Context) {
+	p.stopLock.Lock()
+	defer p.stopLock.Unlock()
+
+	if p.stopped {
+		return
+	}
+
+	ready := new(sync.WaitGroup)
+	ready.Add(p.maxSize)
+
+	for i := 0; i < p.maxSize; i++ {
+		pauseTask := NewSilentTask(
+			func(taskCtx context.Context) error {
+				ready.Done()
+
+				select {
+				case <-taskCtx.Done():
+				case <-p.stopSignal:
+				}
+
+				return nil
+			},
+		)
+
+		p.Submit(ctx, pauseTask)
+	}
+
+	// Wait for workers to all be paused
+	ready.Wait()
+}
+
+// dispatch sends the next queued task to an available worker.
+func (p *WorkerPool) dispatch() {
+	defer close(p.stoppedChan)
+
+	var workerCount int
+	var idle bool
+	var wg sync.WaitGroup
+
+Loop:
+	for {
+		// As long as the waiting queue is not empty, incoming tasks will go directly
+		// into the waiting queue. We'll try to run all tasks from this queue first.
+		// Once the waiting queue is empty, then go back to submitting incoming tasks
+		// directly to available workers.
+		if p.waitingQueue.Len() != 0 {
+			if !p.processWaitingQueue() {
+				break Loop
+			}
+
+			continue
+		}
+
+		select {
+		case task, ok := <-p.taskQueue:
+			if !ok {
+				break Loop
+			}
+
+			// Got a task to do.
+			select {
+			case p.workerQueue <- task:
+			default:
+				// Create a new worker, if not at max.
+				if workerCount < p.maxSize {
+					wg.Add(1)
+					go startWorker(task, p.workerQueue, &wg)
+					workerCount++
+
+				} else {
+					// Enqueue task to be executed by next available worker.
+					p.pushBack(task)
+
+				}
+			}
+
+			idle = false
+
+		case <-time.After(idleTimeout):
+			// Timed out waiting for a new task to arrive. Kill an available worker if
+			// this worker pool has been idle for the whole duration.
+			if idle && workerCount > 0 {
+				if p.killIdleWorker() {
+					workerCount--
+				}
+			}
+
+			idle = true
+		}
+	}
+
+	if p.waitBeforeShutdown {
+		p.drainQueuedTasks(false)
+	} else {
+		p.drainQueuedTasks(true)
+	}
+
+	// Stop all workers as they become available.
+	for workerCount > 0 {
+		p.workerQueue <- nil
+		workerCount--
+	}
+
+	wg.Wait()
+}
+
+// startWorker runs the initial task, then starts a worker waiting for more.
+func startWorker(task *queuedTask, workerQueue chan *queuedTask, wg *sync.WaitGroup) {
+	task.run()
+	worker(workerQueue, wg)
+}
+
+// worker executes tasks and stops when it receives a nil task.
+func worker(workerQueue chan *queuedTask, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for task := range workerQueue {
+		if task == nil {
+			return
+		}
+
+		task.run()
+	}
+}
+
+// stop tells the dispatcher to exit, and whether to complete the queued tasks.
+func (p *WorkerPool) stop(waitBeforeShutdown bool) {
+	p.stopOnce.Do(
+		func() {
+			// Indicate that worker pool is stopping, to unpause all paused workers.
+			close(p.stopSignal)
+			// Acquire stopLock to wait for any pause in progress to complete.
+			p.stopLock.Lock()
+			// The stopped flag prevents this worker pool from being paused again. This
+			// makes it safe to close the taskQueue.
+			p.stopped = true
+			p.stopLock.Unlock()
+			p.waitBeforeShutdown = waitBeforeShutdown
+			// Close task queue and wait for currently running tasks to finish.
+			close(p.taskQueue)
+		},
+	)
+
+	<-p.stoppedChan
+}
+
+// processWaitingQueue puts new tasks onto the waiting queue, and removes tasks from
+// the waiting queue as workers become available. Returns false if this worker pool
+// was stopped.
+func (p *WorkerPool) processWaitingQueue() bool {
+	select {
+	case t, ok := <-p.taskQueue:
+		if !ok {
+			return false
+		}
+
+		p.pushBack(t)
+
+	case p.workerQueue <- p.peekFront():
+		// A task was given to an available worker.
+		p.popFront()
+
+	}
+
+	return true
+}
+
+func (p *WorkerPool) killIdleWorker() bool {
+	select {
+	case p.workerQueue <- nil:
+		// Sent kill signal to worker.
+		return true
+	default:
+		// No ready workers. All, if any, workers are busy.
+		return false
+	}
+}
+
+// drainQueuedTasks continuously pops a task from the waiting queue and either cancel
+// or gives it to an available worker until the queue is empty.
+func (p *WorkerPool) drainQueuedTasks(toCancel bool) {
+	for p.waitingQueue.Len() != 0 {
+		t := p.popFront()
+
+		if toCancel {
+			t.cancel()
+			continue
+		}
+
+		// Give a task to the next available worker
+		p.workerQueue <- t
+	}
+}
+
+// pushBack pushes a task to the back of the queue
+func (p *WorkerPool) pushBack(task *queuedTask) {
+	p.waitingQueue.PushBack(task)
+	atomic.StoreInt32(&p.pendingSize, int32(p.waitingQueue.Len()))
+}
+
+// popFront removes and returns the task at the front of the queue
+func (p *WorkerPool) popFront() *queuedTask {
+	t := p.waitingQueue.PopFront().(*queuedTask)
+	atomic.StoreInt32(&p.pendingSize, int32(p.waitingQueue.Len()))
+
+	return t
+}
+
+// peekFront returns but not removes the task at the front of the queue
+func (p *WorkerPool) peekFront() *queuedTask {
+	return p.waitingQueue.Front().(*queuedTask)
+}
---
+++ async/worker_pool_test.go
@@ -0,0 +1,690 @@
+package async
+
+import (
+	"context"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+)
+
+const max = 20
+
+func TestExample(t *testing.T) {
+	wp := New(2)
+	requests := []string{"alpha", "beta", "gamma", "delta", "epsilon"}
+
+	rspChan := make(chan string, len(requests))
+	for _, r := range requests {
+		r := r
+		wp.Submit(
+			context.Background(), NewSilentTask(
+				func(ctx context.Context) error {
+					rspChan <- r
+					return nil
+				},
+			),
+		)
+	}
+
+	wp.StopWait()
+	close(rspChan)
+
+	rspSet := map[string]struct{}{}
+	for rsp := range rspChan {
+		rspSet[rsp] = struct{}{}
+	}
+
+	if len(rspSet) < len(requests) {
+		t.Fatal("Did not handle all requests")
+	}
+
+	for _, req := range requests {
+		if _, ok := rspSet[req]; !ok {
+			t.Fatal("Missing expected values:", req)
+		}
+	}
+}
+
+func TestMaxWorkers(t *testing.T) {
+	wp := New(0)
+	wp.Stop()
+
+	if wp.maxSize != runtime.NumCPU() {
+		t.Fatal("should have created some workers")
+	}
+
+	wp = New(max)
+	defer wp.Stop()
+
+	if wp.Size() != max {
+		t.Fatal("wrong size returned")
+	}
+
+	started := make(chan struct{}, max)
+	release := make(chan struct{})
+
+	// Start workers, and have them all wait on a channel before completing.
+	for i := 0; i < max; i++ {
+		wp.Submit(
+			context.Background(), NewSilentTask(
+				func(ctx context.Context) error {
+					started <- struct{}{}
+					<-release
+					return nil
+				},
+			),
+		)
+	}
+
+	// Wait for all queued tasks to be dispatched to workers.
+	if wp.waitingQueue.Len() != wp.WaitingQueueSize() {
+		t.Fatal("Working Queue size returned should not be 0")
+	}
+
+	timeout := time.After(5 * time.Second)
+	for startCount := 0; startCount < max; {
+		select {
+		case <-started:
+			startCount++
+		case <-timeout:
+			t.Fatal("timed out waiting for workers to start")
+		}
+	}
+
+	// Release workers.
+	close(release)
+}
+
+func TestReuseWorkers(t *testing.T) {
+	wp := New(5)
+	defer wp.Stop()
+
+	release := make(chan struct{})
+
+	// Cause worker to be created, and available for reuse before next task.
+	for i := 0; i < 1; i++ {
+		wp.Submit(
+			context.Background(), NewSilentTask(
+				func(ctx context.Context) error {
+					<-release
+					return nil
+				},
+			),
+		)
+
+		release <- struct{}{}
+		time.Sleep(time.Millisecond)
+	}
+
+	close(release)
+
+	// If the same worker was always reused, then only one worker would have
+	// been created and there should only be one ready.
+	if countReady(wp) > 1 {
+		t.Fatal("Worker not reused")
+	}
+}
+
+func TestWorkerTimeout(t *testing.T) {
+	wp := New(max)
+	defer wp.Stop()
+
+	// Start workers, and have them all wait on ctx before completing.
+	ctx, cancel := context.WithCancel(context.Background())
+	wp.Pause(ctx)
+
+	if anyReady(wp) {
+		t.Fatal("number of ready workers should be zero")
+	}
+
+	if wp.killIdleWorker() {
+		t.Fatal("should have been no idle workers to kill")
+	}
+
+	// Release workers.
+	cancel()
+
+	if countReady(wp) != max {
+		t.Fatal("Expected", max, "ready workers")
+	}
+
+	// Check that a worker timed out.
+	time.Sleep(idleTimeout*2 + idleTimeout/2)
+	if countReady(wp) != max-1 {
+		t.Fatal("First worker did not timeout")
+	}
+
+	// Check that another worker timed out.
+	time.Sleep(idleTimeout)
+	if countReady(wp) != max-2 {
+		t.Fatal("Second worker did not timeout")
+	}
+}
+
+func TestStop(t *testing.T) {
+	wp := New(max)
+
+	// Start workers, and have them all wait on ctx before completing.
+	ctx, cancel := context.WithCancel(context.Background())
+	wp.Pause(ctx)
+
+	// Release workers.
+	cancel()
+
+	if wp.Stopped() {
+		t.Fatal("pool should not be stopped")
+	}
+
+	wp.Stop()
+	if anyReady(wp) {
+		t.Fatal("should have zero workers after stop")
+	}
+
+	if !wp.Stopped() {
+		t.Fatal("pool should be stopped")
+	}
+
+	// Start workers, and have them all wait on a channel before completing.
+	wp = New(5)
+
+	release := make(chan struct{})
+	finished := make(chan struct{}, max)
+
+	for i := 0; i < max; i++ {
+		wp.Submit(
+			context.Background(), NewSilentTask(
+				func(ctx context.Context) error {
+					<-release
+					finished <- struct{}{}
+					return nil
+				},
+			),
+		)
+	}
+
+	// Call Stop() and see that only the already running tasks were completed.
+	go func() {
+		time.Sleep(10 * time.Millisecond)
+		close(release)
+	}()
+
+	wp.Stop()
+	var count int
+
+Count:
+	for count < max {
+		select {
+		case <-finished:
+			count++
+		default:
+			break Count
+		}
+	}
+
+	if count > 5 {
+		t.Fatal("Should not have completed any queued tasks, did", count)
+	}
+
+	// Check that calling Stop() again is OK.
+	wp.Stop()
+}
+
+func TestStopWait(t *testing.T) {
+	// Start workers, and have them all wait on a channel before completing.
+	wp := New(5)
+	release := make(chan struct{})
+	finished := make(chan struct{}, max)
+
+	for i := 0; i < max; i++ {
+		wp.Submit(
+			context.Background(), NewSilentTask(
+				func(ctx context.Context) error {
+					<-release
+					finished <- struct{}{}
+					return nil
+				},
+			),
+		)
+	}
+
+	// Call StopWait() and see that all tasks were completed.
+	go func() {
+		time.Sleep(10 * time.Millisecond)
+		close(release)
+	}()
+
+	wp.StopWait()
+
+	for count := 0; count < max; count++ {
+		select {
+		case <-finished:
+		default:
+			t.Fatal("Should have completed all queued tasks")
+		}
+	}
+
+	if anyReady(wp) {
+		t.Fatal("should have zero workers after stopwait")
+	}
+
+	if !wp.Stopped() {
+		t.Fatal("pool should be stopped")
+	}
+
+	// Make sure that calling StopWait() with no queued tasks is OK.
+	wp = New(5)
+	wp.StopWait()
+
+	if anyReady(wp) {
+		t.Fatal("should have zero workers after stopwait")
+	}
+
+	// Check that calling StopWait() again is OK.
+	wp.StopWait()
+}
+
+func TestDrainingTasks(t *testing.T) {
+	wp := New(2)
+	defer wp.Stop()
+
+	releaseChan := make(chan struct{})
+
+	// Start workers, and have them all wait on a channel before completing.
+	for i := 0; i < 64; i++ {
+		wp.Submit(
+			context.Background(), NewSilentTask(
+				func(ctx context.Context) error {
+					<-releaseChan
+					return nil
+				},
+			),
+		)
+	}
+
+	// Start a goroutine to free the workers after calling stop. This way
+	// the dispatcher can exit, then when this goroutine runs, the workerpool
+	// can exit.
+	go func() {
+		<-time.After(time.Millisecond)
+		close(releaseChan)
+	}()
+
+	wp.Stop()
+
+	// Now that the worker pool has exited, it is safe to inspect its waiting
+	// queue without causing a race.
+	qlen := wp.waitingQueue.Len()
+	if qlen != 0 {
+		t.Fatal("Expected all tasks to have been drained, have", qlen)
+	}
+}
+
+func TestStopRace(t *testing.T) {
+	wp := New(max)
+	defer wp.Stop()
+
+	workRelChan := make(chan struct{})
+
+	var started sync.WaitGroup
+	started.Add(max)
+
+	// Start workers, and have them all wait on a channel before completing.
+	for i := 0; i < max; i++ {
+		wp.Submit(
+			context.Background(), NewSilentTask(
+				func(ctx context.Context) error {
+					started.Done()
+					<-workRelChan
+					return nil
+				},
+			),
+		)
+	}
+
+	started.Wait()
+
+	const doneCallers = 5
+	stopDone := make(chan struct{}, doneCallers)
+
+	for i := 0; i < doneCallers; i++ {
+		go func() {
+			wp.Stop()
+			stopDone <- struct{}{}
+		}()
+	}
+
+	select {
+	case <-stopDone:
+		t.Fatal("Stop should not return in any goroutine")
+	default:
+	}
+
+	close(workRelChan)
+
+	timeout := time.After(time.Second)
+	for i := 0; i < doneCallers; i++ {
+		select {
+		case <-stopDone:
+		case <-timeout:
+			wp.Stop()
+			t.Fatal("timedout waiting for Stop to return")
+		}
+	}
+}
+
+// Run this test with race detector to test that using WaitingQueueSize has no
+// race condition
+func TestWaitingQueueSizeRace(t *testing.T) {
+	const (
+		goroutines = 10
+		tasks      = 20
+		workers    = 5
+	)
+
+	wp := New(workers)
+	defer wp.Stop()
+
+	maxChan := make(chan int)
+	for g := 0; g < goroutines; g++ {
+		go func() {
+			max := 0
+
+			// Submit 100 tasks, checking waiting queue size each time.  Report
+			// the maximum queue size seen.
+			for i := 0; i < tasks; i++ {
+				wp.Submit(
+					context.Background(), NewSilentTask(
+						func(ctx context.Context) error {
+							time.Sleep(time.Microsecond)
+							return nil
+						},
+					),
+				)
+
+				waiting := wp.WaitingQueueSize()
+				if waiting > max {
+					max = waiting
+				}
+			}
+
+			maxChan <- max
+		}()
+	}
+
+	// Find maximum queuesize seen by any goroutine.
+	maxMax := 0
+	for g := 0; g < goroutines; g++ {
+		max := <-maxChan
+		if max > maxMax {
+			maxMax = max
+		}
+	}
+
+	if maxMax == 0 {
+		t.Error("expected to see waiting queue size > 0")
+	}
+
+	if maxMax >= goroutines*tasks {
+		t.Error("should not have seen all tasks on waiting queue")
+	}
+}
+
+func TestPause(t *testing.T) {
+	wp := New(25)
+	defer wp.Stop()
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	ran := make(chan struct{})
+
+	wp.Submit(
+		context.Background(), NewSilentTask(
+			func(ctx context.Context) error {
+				time.Sleep(time.Millisecond)
+				close(ran)
+				return nil
+			},
+		),
+	)
+
+	wp.Pause(ctx)
+
+	// Check that Pause waits for all previously submitted tasks to run.
+	select {
+	case <-ran:
+	default:
+		t.Error("did not run all tasks before returning from Pause")
+	}
+
+	ran = make(chan struct{})
+
+	wp.Submit(
+		context.Background(), NewSilentTask(
+			func(ctx context.Context) error {
+				close(ran)
+				return nil
+			},
+		),
+	)
+
+	// Check that a new task did not run while paused
+	select {
+	case <-ran:
+		t.Error("ran while paused")
+	case <-time.After(time.Millisecond):
+	}
+
+	// Check that task was enqueued
+	if wp.WaitingQueueSize() != 1 {
+		t.Error("waiting queue size should be 1")
+	}
+
+	// Cancel context to unpause workers.
+	cancel()
+
+	// Check that task was run after unpausing.
+	select {
+	case <-ran:
+	case <-time.After(time.Second):
+		t.Error("did not run after canceling pause")
+	}
+
+	// ---- Test pause while paused
+
+	ctx, cancel = context.WithCancel(context.Background())
+	wp.Pause(ctx)
+
+	ctx2, cancel2 := context.WithCancel(context.Background())
+
+	pauseDone := make(chan struct{})
+	go func() {
+		wp.Pause(ctx2)
+		close(pauseDone)
+	}()
+
+	// Check that second pause does not return until first pause in cancelled
+	select {
+	case <-pauseDone:
+		wp.Stop()
+		t.Fatal("second Pause should not have returned")
+	case <-time.After(time.Millisecond):
+	}
+
+	cancel() // cancel 1st pause
+
+	// Check that second pause returns
+	select {
+	case <-pauseDone:
+	case <-time.After(time.Second):
+		wp.Stop()
+		t.Fatal("timed out waiting for Pause to return")
+	}
+
+	cancel2() // cancel 2nd pause
+
+	// ---- Test concurrent pauses
+
+	ctx, cancel = context.WithCancel(context.Background())
+	ctx2, cancel2 = context.WithCancel(context.Background())
+
+	pauseDone = make(chan struct{})
+	pause2Done := make(chan struct{})
+
+	go func() {
+		wp.Pause(ctx)
+		close(pauseDone)
+	}()
+
+	go func() {
+		wp.Pause(ctx2)
+		close(pause2Done)
+	}()
+
+	select {
+	case <-pauseDone:
+		cancel()
+		<-pause2Done
+		cancel2()
+	case <-pause2Done:
+		cancel2()
+		<-pauseDone
+		cancel()
+	case <-time.After(time.Second):
+		t.Fatal("concurrent pauses deadlocked")
+	}
+
+	// ---- Test stopping paused pool ----
+
+	ctx, cancel = context.WithCancel(context.Background())
+	ctx2, cancel2 = context.WithCancel(context.Background())
+
+	// Stack up two pauses
+	wp.Pause(ctx)
+	go wp.Pause(ctx2)
+
+	ran = make(chan struct{})
+
+	wp.Submit(
+		context.Background(), NewSilentTask(
+			func(ctx context.Context) error {
+				close(ran)
+				return nil
+			},
+		),
+	)
+
+	stopDone := make(chan struct{})
+	go func() {
+		wp.StopWait()
+		close(stopDone)
+	}()
+
+	// Check that task was run after calling StopWait
+	select {
+	case <-stopDone:
+	case <-time.After(time.Second):
+		t.Fatal("timed out waiting for StopWait to return")
+	}
+
+	// Check that task was run after calling StopWait
+	select {
+	case <-ran:
+	default:
+		t.Error("did not run after canceling pause")
+	}
+
+	defer cancel()
+	defer cancel2()
+
+	// ---- Test pause after stop ----
+
+	ctx, cancel = context.WithCancel(context.Background())
+	pauseDone = make(chan struct{})
+
+	go func() {
+		wp.Pause(ctx)
+		close(pauseDone)
+	}()
+
+	select {
+	case <-pauseDone:
+	case <-time.After(time.Second):
+		t.Fatal("pause after stop did not return")
+	}
+
+	cancel()
+}
+
+func TestWorkerLeak(t *testing.T) {
+	const workerCount = 100
+
+	wp := New(workerCount)
+
+	// Start workers, and have them all wait on a channel before completing.
+	for i := 0; i < workerCount; i++ {
+		wp.Submit(
+			context.Background(), NewSilentTask(
+				func(ctx context.Context) error {
+					time.Sleep(time.Millisecond)
+					return nil
+				},
+			),
+		)
+	}
+
+	// If wp..Stop() is not waiting for all workers to complete, then goleak
+	// should catch that
+	wp.Stop()
+}
+
+func anyReady(w *WorkerPool) bool {
+	release := make(chan struct{})
+
+	wait := &queuedTask{
+		task: NewSilentTask(
+			func(ctx context.Context) error {
+				<-release
+				return nil
+			},
+		),
+	}
+
+	select {
+	case w.workerQueue <- wait:
+		close(release)
+		return true
+	default:
+	}
+
+	return false
+}
+
+func countReady(w *WorkerPool) int {
+	// Try to stop max workers.
+	timeout := time.After(100 * time.Millisecond)
+	release := make(chan struct{})
+
+	wait := &queuedTask{
+		ctx: context.Background(),
+		task: NewSilentTask(
+			func(ctx context.Context) error {
+				<-release
+				return nil
+			},
+		),
+	}
+
+	var readyCount int
+	for i := 0; i < max; i++ {
+		select {
+		case w.workerQueue <- wait:
+			readyCount++
+		case <-timeout:
+			i = max
+		}
+	}
+
+	close(release)
+	return readyCount
+}
